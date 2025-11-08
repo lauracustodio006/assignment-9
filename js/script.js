b9863d5240a86c70ad46ae9f47e90aa9cd08efcf
laura <lcustodio@georgetowncollege.edu>
2025-11-07 20:44:25 -0600

    first part

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
 console.log("script.js loaded");
 
-async function getGif() {
-  const response = await fetch("https://api.giphy.com/v1/gifs/search?api_key=jbXBWpHy8jLm8tMcOSl8CB0SOHLYx2dU&q=gif&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips");
-  const data = await response.json();
-  const gift = data.data;
-  
+const button = document.getElementById("fetch-gif-btn");
+const gifContainer = document.getElementById("gif-container");
 
-  const output = document.getElementById("gif-container"); 
-  output.textContent = "";
+const url = "https://api.giphy.com/v1/gifs/search?api_key=jbXBWpHy8jLm8tMcOSl8CB0SOHLYx2dU&q=cat&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
+
+async function getCatGifs() {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  gifContainer.innerHTML = "";
+
+  data.data.forEach(gif => {
+    const img = document.createElement("img");
+    img.src = gif.images.original.url;
+    img.className = "col-3 mb-3";
+    gifContainer.appendChild(img);
+  });
 }
 
-console.log(getGif)
+button.addEventListener("click", getCatGifs);
+
