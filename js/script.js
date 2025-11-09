07bfe63c9b52b248b64791a8a2cf23676e5ed491
laura <lcustodio@georgetowncollege.edu>
2025-11-09 11:58:06 -0600

    final project

--- js/script.js
+++ js/script.js
@@ -3,15 +3,15 @@ console.log("script.js loaded");
 const button = document.getElementById("fetch-gif-btn");
 const gifContainer = document.getElementById("gif-container");
 
-const url = "https://api.giphy.com/v1/gifs/search?api_key=jbXBWpHy8jLm8tMcOSl8CB0SOHLYx2dU&q=cat&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
+const api_ex = "https://api.giphy.com/v1/gifs/search?api_key=jbXBWpHy8jLm8tMcOSl8CB0SOHLYx2dU&q=cat&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
 
 async function getCatGifs() {
-  const response = await fetch(url);
+  const response = await fetch(api_ex);
   const data = await response.json();
 
   gifContainer.innerHTML = "";
 
-  data.data.forEach(gif => {
+  data.data.forEach(function(gif) {
     const img = document.createElement("img");
     img.src = gif.images.original.url;
     img.className = "col-3 mb-3";
